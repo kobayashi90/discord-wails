6aea7ea03aab4ec26c4fc165fb79bc42f8bcade9
Kobayashi <51263756+kobayashi90@users.noreply.github.com>
2025-06-15 20:22:01 +0200

    Update app.go

--- app.go
+++ app.go
@@ -1,14 +1,44 @@
 package main
 
-import "github.com/webview/webview_go"
+import (
+	"context"
+	"log"
+
+	"github.com/wailsapp/wails/v2"
+	"github.com/wailsapp/wails/v2/pkg/options"
+)
+
+// App struct
+type App struct {
+	ctx context.Context
+}
+
+// NewApp creates a new App application struct
+func NewApp() *App {
+	return &App{}
+}
+
+// OnStartup is called when the app starts up
+func (a *App) OnStartup(ctx context.Context) {
+	a.ctx = ctx
+}
 
 func main() {
-    debug := true
-    w := webview.New(debug)
-    defer w.Destroy()
+	// Create an instance of the app structure
+	app := NewApp()
 
-    w.SetTitle("Discord")
-    w.SetSize(1280, 800, webview.HintNone)
-    w.Navigate("https://discord.com/app")
-    w.Run()
+	// Create application with options
+	err := wails.Run(&options.App{
+		Title:            "Discord",
+		Width:            1200,
+		Height:           800,
+		BackgroundColour: &options.RGBA{R: 54, G: 57, B: 63, A: 1}, // Discord's dark theme color
+		OnStartup:        app.OnStartup,
+		WebviewIsTransparent: false,
+		WindowStartState: options.Normal,
+	})
+
+	if err != nil {
+		log.Fatal("Error:", err)
+	}
 }
